283df397fe6df064add76a586edec5d13f804b25
Marat Karimov <karimov_m@inbox.ru>
2021-01-12 03:41:59 +0300

    error message

--- src/main/kotlin/ru/maratk/spring/reactive/example/handlers/BookHandlers.kt
+++ src/main/kotlin/ru/maratk/spring/reactive/example/handlers/BookHandlers.kt
@@ -13,20 +13,20 @@ class BookHandlers(private val bookRepository: BookRepository) {
     suspend fun findById(req: ServerRequest): ServerResponse {
         val bookTry = bookRepository.findById(req.pathVariable("id").toInt()).first()
         return try { ServerResponse.ok().bodyValueAndAwait(bookTry.get()) }
-        catch (e: Exception) { ServerResponse.badRequest().bodyValueAndAwait(e.message!!) }
+        catch (e: Exception) { ServerResponse.badRequest().bodyValueAndAwait(e.message ?: "Error!") }
     }
 
     @FlowPreview
     suspend fun findAll(): ServerResponse {
         val bookTries = bookRepository.findAll().toList()
         return try { ServerResponse.ok().bodyValueAndAwait(bookTries.map { it.get() }) }
-        catch (e: Exception) { ServerResponse.badRequest().bodyValueAndAwait(e.message!!) }
+        catch (e: Exception) { ServerResponse.badRequest().bodyValueAndAwait(e.message ?: "Error!") }
     }
 
     @FlowPreview
     suspend fun add(req: ServerRequest): ServerResponse {
         val idTry = bookRepository.add(req.awaitBody(Book::class)).first()
         return try { ServerResponse.ok().bodyValueAndAwait(idTry.get()) }
-        catch (e: Exception) { ServerResponse.badRequest().bodyValueAndAwait(e.message!!) }
+        catch (e: Exception) { ServerResponse.badRequest().bodyValueAndAwait(e.message ?: "Error!") }
     }
 }
